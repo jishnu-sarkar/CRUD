2e5eab734d9e4abe5ccf1cbf4166a93b280cb87c
jishnu-sarkar <jishnu.sarkar@bitcanny.com>
2022-01-14 14:29:57 +0530

    Adding Assosiation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,16 @@
 // const { getAllUsers } = require("../models/getUser");
+const lodash = require("lodash");
 const db = require("../models");
 
 //Display All Users
 const getUsers = async (req, res) => {
   const result = await db.sequelize.models.User.getUsers();
-  if (result) {
-    // const result_js = result.map((e) => e.toJSON());
-    console.log(result);
+  if (!lodash.isEmpty(result)) {
+    const result_js = result.map((e) => e.toJSON());
+    console.log(result_js);
     return res.status(201).json({ result });
   } else {
-    return res.status(404).json({ message: "Something Went Wrong!!!" });
+    return res.status(404).json({ message: "DataBase Empty!!!" });
   }
 };
 
@@ -29,10 +30,10 @@ const getUser = async (req, res) => {
 
 //Insert Users
 const createUsers = async (req, res) => {
-  if (req.body != {}) {
+  if (lodash.isEmpty(req.body)) {
     console.log(req.body);
+    return res.status(404).json({ message: "Unsufficient Data!!!" });
   }
-
   const data_user = {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
@@ -52,6 +53,9 @@ const createUsers = async (req, res) => {
 
 //Update User
 const update = async (req, res) => {
+  if (lodash.isEmpty(req.body)) {
+    res.status(404).json({ message: "Nothing to Update" });
+  }
   const id = req.params.id;
   const updatedValue = {
     firstName: req.body.firstName,
@@ -74,17 +78,8 @@ const update = async (req, res) => {
 
 //Delete ALL Users
 const delUsers = async (req, res) => {
-  // const id = req.params.id;
   const result = await db.sequelize.models.User.deleteUsers();
   return res.status(201).json({ Deleted: "All Record Deleted" });
-  // if (result) {
-  //   // const result_js = result.map((e) => e.toJSON());
-  //   // console.log(result_js);
-  //   // console.log(result);
-  //   return res.status(201).json({ Deleted: "All Record Deleted" });
-  // } else {
-  //   return res.status(404).json({ message: "User not found!!!" });
-  // }
 };
 
 //Delete Particular User
